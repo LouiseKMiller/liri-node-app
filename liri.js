7ce312bd75a98e955835f23262ce4c20bab25a9d
LouiseKMiller <louise@txmiller.com>
2016-08-15 22:55:19 -0500

    corrected error console logs, modularized

--- liri.js
+++ liri.js
@@ -23,6 +23,86 @@ var userInput = process.argv;
 var command = "";
 var param = "";
 
+// BONUS - output data to the terminal and to a txt file called log.txt
+// each command run gets appended to the log.txt file
+// log.txt file is not overwritten each time a command is run
+function outToBoth(output){
+	fs.appendFileSync("log.txt", output+"\r\n");
+	console.log(output);
+};
+
+function header(title){
+	outToBoth(" ");
+	outToBoth(" ");
+	outToBoth("**************************************************");
+	outToBoth("**             " + title);
+	outToBoth("**************************************************");
+	outToBoth(" ");
+};
+
+function twitterRequest(localParam){
+	// call Twitter API using npm twitter; display results in terminal
+	client.get('statuses/user_timeline', localParam, function(err, tweets, response){
+		if (!err){
+			for (var i=0; i<tweets.length; i++) {
+				outToBoth("@" + tweets[i].in_reply_to_screen_name + " " + tweets[i].text);
+				outToBoth(tweets[i].created_at);
+			}
+		} else {
+			outToBoth('Error occurred:', err);
+		}
+	});
+}
+
+function spotifyRequest(localParam){
+	trackName = "track:" + localParam;		
+	spotify.search({type: 'track', query: trackName}, function(err, data){
+		if (!err) {
+			for (var i=0; i<data.tracks.items.length; i++) {
+				for (var j=0; j<data.tracks.items[i].artists.length; j++) {
+					outToBoth("artist #" + (j+1) + ": " + data.tracks.items[i].artists[j].name);
+				};
+				outToBoth("song name: " + data.tracks.items[i].name);
+				outToBoth("preview link: " + data.tracks.items[i].preview_url);
+				outToBoth("album: " + data.tracks.items[i].album.name);
+				outToBoth(" ");
+			}
+
+		} else {
+			outToBoth('Error occurred:',  err);
+		}
+	});
+}
+
+function movieRequest(name){
+
+
+	// define queryURL for movie search
+	// include rotton tomatoes info
+	var queryURL = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&r=json&tomatoes=true&type=movie";
+
+	// use request package to create and send API call for the specific movie 
+	request(queryURL, function(err, response, body) {
+		if (!err && response.statusCode == 200) {
+			outToBoth("Title: " + JSON.parse(body)["Title"]);
+			outToBoth("Year: " + JSON.parse(body)["Year"]);
+			outToBoth("iMDB Rating: " + JSON.parse(body)["imdbRating"]);
+			outToBoth("Country: " + JSON.parse(body)["Country"]);
+			outToBoth("Language: " + JSON.parse(body)["Language"]);
+			outToBoth("Plot: " + JSON.parse(body)["Plot"]);
+			outToBoth("Actors: " + JSON.parse(body)["Actors"]);
+			outToBoth("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
+			outToBoth("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+		} else {
+			outToBoth('Error occurred:', err);			
+		}
+	});
+}
+
+// *********************************************************************
+// START OF CODE EXECUTION
+// *********************************************************************
+
 // first check if instruction should be read from command line or random.txt
 if (userInput[2] == "do-what-it-says") {
 
@@ -46,22 +126,6 @@ if (userInput[2] == "do-what-it-says") {
 	};
 };
 
-// BONUS - output data to the terminal and to a txt file called log.txt
-// each command run gets appended to the log.txt file
-// log.txt file is not overwritten each time a command is run
-function outToBoth(output){
-	fs.appendFileSync("log.txt", output+"\r\n");
-	console.log(output);
-};
-
-function header(title){
-	outToBoth(" ");
-	outToBoth(" ");
-	outToBoth("**************************************************");
-	outToBoth("**             " + title);
-	outToBoth("**************************************************");
-	outToBoth(" ");
-};
 
 // switch statement for three possible commands
 switch(command) {
@@ -81,18 +145,10 @@ switch(command) {
 			consumer_secret: keys.twitterkeys.consumer_secret,
 			access_token_key: keys.twitterkeys.access_token_key,
 			access_token_secret: keys.twitterkeys.access_token_secret
-		});
-		// call Twitter API using npm twitter; display results in terminal
-		client.get('statuses/user_timeline', param, function(err, tweets, response){
-			if (!err){
-				for (var i=0; i<tweets.length; i++) {
-					outToBoth("@" + tweets[i].in_reply_to_screen_name + " " + tweets[i].text);
-					outToBoth(tweets[i].created_at);
-				}
-			} else {
-				outToBoth('Error occurred:' +err);
-			}
-		});
+		});		
+
+		// call twitter function
+		twitterRequest(param);
 		break;
 
 	case "spotify-this-song":
@@ -105,24 +161,7 @@ switch(command) {
 			// if user did, search for a track in Spotify database using Spotify API
 			// use use "track" field filter to limit results to tracks with
 			// name containing user specified trackName
-			trackName = "track:" + param;		
-			spotify.search({type: 'track', query: trackName}, function(err, data){
-				if (!err) {
-					for (var i=0; i<data.tracks.items.length; i++) {
-						for (var j=0; j<data.tracks.items[i].artists.length; j++) {
-							outToBoth("artist #" + (j+1) + ": " + data.tracks.items[i].artists[j].name);
-						};
-						outToBoth("song name: " + data.tracks.items[i].name);
-						outToBoth("preview link: " + data.tracks.items[i].preview_url);
-						outToBoth("album: " + data.tracks.items[i].album.name);
-						outToBoth(" ");
-					}
-
-				} else {
-					outToBoth('Error occurred:' + err);
-				}
-			});
-
+			spotifyRequest(param);
 		} else {
 		// if use did not provie a param (i.e., specify a song) then program defaults to "what's my age again" by blink 182
 			outToBoth("\"what's my age again\" by blink 182");
@@ -134,31 +173,12 @@ switch(command) {
 		//  output section heading to console.log and log.txt
 		header(command);
 
-		// OMDb API - Search for a movie in the OMDb database
 		// Check to see if user specified a movie (does param exist?)
 		// If not, default is Mr. Nobody
 		(param!=="") ? (movieName = param) : (movieName = "Mr. Nobody");
 
-		// define queryURL for movie search
-		// include rotton tomatoes info
-		var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json&tomatoes=true&type=movie";
-
-		// use request package to create and send API call for the specific movie 
-		request(queryURL, function(err, response, body) {
-			if (!err && response.statusCode == 200) {
-				outToBoth("Title: " + JSON.parse(body)["Title"]);
-				outToBoth("Year: " + JSON.parse(body)["Year"]);
-				outToBoth("iMDB Rating: " + JSON.parse(body)["imdbRating"]);
-				outToBoth("Country: " + JSON.parse(body)["Country"]);
-				outToBoth("Language: " + JSON.parse(body)["Language"]);
-				outToBoth("Plot: " + JSON.parse(body)["Plot"]);
-				outToBoth("Actors: " + JSON.parse(body)["Actors"]);
-				outToBoth("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
-				outToBoth("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
-			} else {
-				outToBoth('Error occurred:' + err);			
-			}
-		});
+		// Search for a movie in the OMDb database
+		movieRequest(movieName);
 		break;
 
 	
